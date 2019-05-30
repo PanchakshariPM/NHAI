fefca1029bd13ad248057cab9e259329b64fb07e
AjeetSR <ajeetsreddy@gmail.com>
2019-05-30 16:17:05 +0530

    Fixed progress bar issue and responsive height

--- src/app/toll-count-info/toll-count-info.component.ts
+++ src/app/toll-count-info/toll-count-info.component.ts
@@ -19,12 +19,17 @@ export class TollCountInfoComponent implements OnInit {
   };
   public totalTollPlazas = '590';
 
+  public lowProgessBarWidth = 0;
+  public highProgessBarWidth = 0;
+  public moderateProgessBarWidth = 0;
 
 
   constructor(public dashboardService: DashboardService) {
     this.dashboardService.dashboardStatResponse.subscribe((res) => {
-      console.log(res);
       this.statsData = res ? res : this.statsData;
+      this.lowProgessBarWidth = (this.statsData.data.low.now * 100) / this.statsData.total;
+      this.highProgessBarWidth = (this.statsData.data.high.now * 100) / this.statsData.total;
+      this.moderateProgessBarWidth = (this.statsData.data.moderate.now * 100) / this.statsData.total;
     });
   }
 
